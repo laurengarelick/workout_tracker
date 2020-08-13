fcc18f134923a52292c28a903c4d29606089d5e4
lauren garelick <laurengarelick@gmail.com>
2020-08-13 10:25:15 -0700

    ok

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use("/api", require('./routes/apiRoutes'));
 
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
