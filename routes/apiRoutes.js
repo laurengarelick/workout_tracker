c9036fc31ef1d0a211eab021cc261f97da8561cd
lauren garelick <laurengarelick@gmail.com>
2020-08-13 10:44:51 -0700

    added routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,7 +6,16 @@ router.get('/workouts', (req,res)=> {
     Workouts.find().then(data=> res.json(data)).catch(err=>console.log(err))
 });
 
-router.get('/stats', (req,res)=> {
+router.post("/workouts", (req,res)=>{
+    Workouts.create({}).then(data=> res.json(data))
+})
+
+router.put("/workouts/:id", (req,res)=>{
+    Workouts.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}})
+    .then(data=> res.json(data))
+})
+
+router.get('/workouts/range', (req,res)=> {
     console.log("getting info")
     Workouts.find().then(data=> res.json(data)).catch(err=>console.log(err))
 });
