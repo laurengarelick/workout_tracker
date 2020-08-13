4c46bc8df13095c345a3ed72d6b0606bab3f6010
lauren garelick <laurengarelick@gmail.com>
2020-08-13 00:31:48 -0700

    small details

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 app.use(require('./routes/htmlRoutes'));
 app.use("/api", require('./routes/apiRoutes'));
